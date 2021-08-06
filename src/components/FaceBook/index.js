3881cd31c392cdd3a77b05ad7a0d0ada1aad7512
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-06 09:55:39 -0500

    added search, filter, and sort

--- src/components/FaceBook/index.js
+++ src/components/FaceBook/index.js
@@ -13,14 +13,99 @@ export default function FaceBook() {
   });
 
   const [currentCountry, setCurrentCountry] = useState('All');
+  const [profilesList, setProfilesList] = useState(profiles);
+  const [searchString, setSearchString] = useState('');
 
   const handleClick = (e) => {
     e.preventDefault();
 
     setCurrentCountry(e.target.value);
   };
+
+  const sortByFirstName = (e) => {
+    e.preventDefault();
+
+    const sortedArr = [...profilesList].sort((a, b) => {
+      return a.firstName.localeCompare(b.firstName);
+    });
+    setProfilesList(sortedArr);
+  };
+
+  const sortByLastName = (e) => {
+    e.preventDefault();
+
+    const sortedArr = [...profilesList].sort((a, b) => {
+      return a.lastName.localeCompare(b.lastName);
+    });
+    setProfilesList(sortedArr);
+  };
+
+  const sortByCountry = (e) => {
+    e.preventDefault();
+
+    const sortedArr = [...profilesList].sort((a, b) => {
+      return a.country.localeCompare(b.country);
+    });
+    setProfilesList(sortedArr);
+  };
+
+  const sortByType = (e) => {
+    e.preventDefault();
+
+    const sortedArr = [...profilesList].sort((a, b) => {
+      return a.isStudent - b.isStudent;
+    });
+    setProfilesList(sortedArr);
+  };
+
   return (
     <>
+      <p style={{ marginBottom: '10px' }}>Search:</p>
+      <input
+        style={{ width: '500px' }}
+        value={searchString}
+        onChange={(e) => {
+          setSearchString(e.target.value);
+        }}
+      ></input>
+      <hr />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        Sort by:
+        <button
+          style={{ margin: '0 5px' }}
+          onClick={(e) => {
+            sortByFirstName(e);
+          }}
+        >
+          First Name
+        </button>
+        <button
+          style={{ margin: '0 5px' }}
+          onClick={(e) => {
+            sortByLastName(e);
+          }}
+        >
+          Last Name
+        </button>
+        <button
+          style={{ margin: '0 5px' }}
+          onClick={(e) => {
+            sortByCountry(e);
+          }}
+        >
+          Country
+        </button>
+        <button
+          style={{ margin: '0 5px' }}
+          onClick={(e) => {
+            sortByType(e);
+          }}
+        >
+          Type
+        </button>
+      </div>
+      <hr />
+      <p>Highlight:</p>
       <ul className="buttons">
         {countries.map((country, i) => {
           return (
@@ -42,19 +127,27 @@ export default function FaceBook() {
         })}
       </ul>
       <ul className="facebook">
-        {profiles.map((profile, i) => {
-          return (
-            <FaceBookCard
-              key={i}
-              firstName={profile.firstName}
-              lastName={profile.lastName}
-              country={profile.country}
-              img={profile.img}
-              isStudent={profile.isStudent}
-              currentCountry={currentCountry}
-            />
-          );
-        })}
+        {profilesList
+          .filter((p) => {
+            return (
+              p.firstName.includes(searchString) ||
+              p.lastName.includes(searchString) ||
+              p.country.includes(searchString)
+            );
+          })
+          .map((profile, i) => {
+            return (
+              <FaceBookCard
+                key={i}
+                firstName={profile.firstName}
+                lastName={profile.lastName}
+                country={profile.country}
+                img={profile.img}
+                isStudent={profile.isStudent}
+                currentCountry={currentCountry}
+              />
+            );
+          })}
       </ul>
     </>
   );
--- src/components/FaceBookCard/index.js
+++ src/components/FaceBookCard/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function FaceBookCard(props) {
+  const [visible, setVisible] = useState(true);
+
+  const flipVisible = (e) => {
+    e.preventDefault();
+    const opp = !visible;
+    setVisible(opp);
+  };
   return (
     <div
       className="box fb-box"
@@ -11,25 +18,36 @@ export default function FaceBookCard(props) {
           : {}
       }
     >
-      <img src={props.img} alt="profile" />
-      <div>
-        <p>
-          <b>First name: </b>
-          {props.firstName}
-        </p>
-        <p>
-          <b>Last name: </b>
-          {props.lastName}
-        </p>
-        <p>
-          <b>Country: </b>
-          {props.country}
-        </p>
-        <p>
-          <b>Type: </b>
-          {props.isStudent ? 'Student' : 'Teacher'}
-        </p>
-      </div>
+      <button
+        className="profile-img"
+        onClick={(e) => {
+          flipVisible(e);
+        }}
+      >
+        <img src={props.img} alt="profile" />
+      </button>
+      {visible ? (
+        <div>
+          <p>
+            <b>First name: </b>
+            {props.firstName}
+          </p>
+          <p>
+            <b>Last name: </b>
+            {props.lastName}
+          </p>
+          <p>
+            <b>Country: </b>
+            {props.country}
+          </p>
+          <p>
+            <b>Type: </b>
+            {props.isStudent ? 'Student' : 'Teacher'}
+          </p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
